e4adc0c545fece746ef5ce9a4dcb982c18040658
Logan Traffas <logan.traffas@gmail.com>
2021-02-01 21:37:50 -0800

    Begin work on empires list page

--- server.js
+++ server.js
@@ -8,12 +8,33 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
+function createDefaultContext(name) {
+	var active = {};
+	active[name] = true;
+	return {
+		active: active
+	};
+}
+
+var empires = [
+    {
+        name: "The Allied Suns",
+        aggressiveness: "passive",
+        primary_color: "FF0000",
+        secondary_color: "00FF00",
+        fallen_empire: false
+    }
+];
+
 app.get("/", (req, res, next) => {
 	res.status(200).render("homePage");
 });
 
 app.get("/empires", (req, res, next) => {
-	res.status(200).render("empiresPage");
+	var pageName = "empiresPage";
+	var context = createDefaultContext(pageName);
+	context.empires = empires;
+	res.status(200).render(pageName, context);
 });
 
 app.get("/systems", (req, res, next) => {
