96fed2759cea4109bb5a4c10f521fb2ace57dff8
Matthew Macovsky <macovsky.m@gmail.com>
2021-02-02 12:58:28 -0800

    Make body creation dialog functional on frontend

--- precompile.sh
+++ precompile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 handlebars views/partials/empireItem.handlebars -f public/templates/empire_item_template.js
 handlebars views/partials/systemItem.handlebars -f public/templates/system_item_template.js
+handlebars views/partials/bodyItem.handlebars -f public/templates/body_item_template.js
+handlebars views/partials/resourceItem.handlebars -f public/templates/resource_item_template.js
