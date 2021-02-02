1ab712af9879244e91c8f16027a96a5389b83023
Logan Traffas <traffasl@oregonstate.edu>
2021-02-01 20:30:19 -0800

    Installed forever

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = 3845;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
