35ea6c07c9d8a42a0f490a5d6239f2ff6ad26ca2
Logan Traffas <logan.traffas@gmail.com>
2021-02-01 20:02:39 -0800

    Switch to express handlebars

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-	res.status(200).sendFile(__dirname + '/public/index.html');
+	res.status(200).render("homePage");
+});
+
+app.get("/empires", (req, res, next) => {
+	res.status(200).render("empiresPage");
+});
+
+app.get("/systems", (req, res, next) => {
+	res.status(200).render("systemsPage");
 });
 
 app.get('*', (req, res) => {
-	res.status(404).send("The page you requested doesn't exist");
+	res.status(404).send("The page you requested doesn't exist");1
 });
 
 app.listen(port, () => {
