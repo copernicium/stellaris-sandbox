8d1799bb883a0d7b3167050f133016d45c87f3ea
Matthew Macovsky <macovskm@oregonstate.edu>
2021-02-14 18:19:07 -0800

    Create DELETE data manipulation queries

--- queries/data_manipulation_queries.sql
+++ queries/data_manipulation_queries.sql
@@ -2,6 +2,7 @@
 
 -- Empires list
 SELECT * FROM empires;
+DELETE FROM empires WHERE empireID = ":empireID";
 
 -- Individual empire view
 SELECT * FROM empires WHERE empireID = ":empireID";
@@ -12,12 +13,14 @@ INSERT INTO resource_stocks (empireID, resourceID, quantity) VALUES (":empireID"
 UPDATE empires SET name=":empireName", aggressiveness=":empireAggressiveness", primaryColor=":empirePrimaryColor", secondaryColor=":empireSecondaryColor", isFallenEmpire=":empireIsFallenEmpire" WHERE empireID = ":empireID";
 UPDATE resource_stocks SET quantity=":quantity" WHERE empireID = ":empireID" AND resourceID = ":resourceID";
 UPDATE systems SET empireID=":empireID" WHERE systemID = ":systemID";
+DELETE FROM resource_stocks WHERE resourceID = ":resourceID" AND empireID = ":empireID";
 
 -- Empire search (other pages)
 SELECT empireID, name FROM empires WHERE name LIKE "%:searchQuery%";
 
 -- Systems list
 SELECT * FROM systems;
+DELETE FROM systems WHERE systemID = ":systemID";
 
 -- Individual system view
 SELECT * FROM systems WHERE systemID = ":systemID";
@@ -33,6 +36,7 @@ SELECT systems.systemID, systems.name FROM systems WHERE systems.name LIKE "%:se
 
 -- Bodies list
 SELECT * FROM bodies;
+DELETE FROM bodies WHERE bodyID = ":bodyID";
 
 -- Individual body view
 SELECT * FROM bodies WHERE bodyID = ":bodyID";
@@ -41,9 +45,11 @@ INSERT INTO bodies (name, type, theta, orbitalRadius, systemID) VALUES (":bodyNa
 INSERT INTO resource_deposits (bodyID, resourceID, quantity) VALUES (":bodyID", ":resourceID", ":quantity");
 UPDATE bodies SET name=":bodyName", type=":bodyType", theta=":bodyTheta", orbitalRadius=":bodyOrbitalRadius" WHERE bodyID = ":bodyID";
 UPDATE resource_deposits SET quantity=":quantity" WHERE bodyID = ":bodyID" AND resourceID = ":resourceID";
+DELETE FROM resource_deposits WHERE resourceID = ":resourceID" AND bodyID = ":bodyID";
 
 -- Resources list
 SELECT * FROM resources;
+DELETE FROM resources WHERE resourceID = ":resourceID";
 
 -- Individual resource view
 SELECT * FROM resources WHERE resourceID = ":resourceID";
@@ -56,6 +62,7 @@ SELECT resources.resourceID, resources.name FROM resources WHERE resources.name 
 -- Hyperlanes list
 SELECT s1.system1ID, s1.system2ID, s1.name AS system1Name, s2.name AS system2Name, s1.orbitalRadius AS system1OrbitalRadius, s1.theta AS system1Theta, s2.orbitalRadius AS system2OrbitalRadius, s2.theta AS system2Theta FROM (SELECT hyperlanes.system1ID, hyperlanes.system2ID, systems.name, systems.orbitalRadius, systems.theta FROM hyperlanes INNER JOIN systems ON hyperlanes.system1ID = systems.systemID) AS s1 INNER JOIN (SELECT hyperlanes.system1ID, hyperlanes.system2ID, systems.name, systems.orbitalRadius, systems.theta FROM hyperlanes INNER JOIN systems ON hyperlanes.system2ID = systems.systemID) AS s2 ON s1.system1ID = s2.system1ID AND s1.system2ID = s2.system2ID;
 INSERT INTO hyperlanes (system1ID, system2ID) VALUES (":system1ID", ":system2ID");
+DELETE FROM hyperlanes WHERE system1ID = ":system1ID" AND system2ID = ":system2ID";
 
 -- Individual hyperlane view
 SELECT s1.name AS system1Name, s2.name AS system2Name FROM (SELECT hyperlanes.system1ID, hyperlanes.system2ID, systems.name FROM hyperlanes INNER JOIN systems ON hyperlanes.system1ID = systems.systemID) AS s1 INNER JOIN (SELECT hyperlanes.system1ID, hyperlanes.system2ID, systems.name FROM hyperlanes INNER JOIN systems ON hyperlanes.system2ID = systems.systemID) AS s2 ON s1.system1ID = s2.system1ID AND s1.system2ID = s2.system2ID WHERE s1.system1ID = ":system1ID" AND s2.system2ID = ":system2ID";
