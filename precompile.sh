85f42e703f2961b4fccbb5fac02ff53663b23285
Matthew Macovsky <macovsky.m@gmail.com>
2021-02-18 14:35:21 -0800

    Remove obsolete precompilation steps

--- precompile.sh
+++ precompile.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 mkdir -p ./public/templates
 
-handlebars views/partials/empireItem.handlebars -f public/templates/empire_item_template.js
-handlebars views/partials/systemItem.handlebars -f public/templates/system_item_template.js
-handlebars views/partials/bodyItem.handlebars -f public/templates/body_item_template.js
-handlebars views/partials/resourceItem.handlebars -f public/templates/resource_item_template.js
 handlebars views/partials/hyperlaneItem.handlebars -f public/templates/hyperlane_item_template.js
 handlebars views/partials/modalBackdrop.handlebars -f public/templates/modal_backdrop.js
 handlebars views/partials/confirmationModal.handlebars -f public/templates/confirmation_modal.js
